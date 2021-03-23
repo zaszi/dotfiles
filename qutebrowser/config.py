bc2d7b30f314bf067e3938c0c31ab1ac80d19571
zaszi <admin@zaszi.net>
2021-03-23 21:08:59 +0100

    Update qutebrowser config

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -417,6 +417,14 @@ c.colors.tabs.selected.even.bg = base02
 ## Type: Bool
 c.colors.webpage.darkmode.enabled = True
 
-## Force `prefers-color-scheme: dark` colors for websites.
-## Type: Bool
-c.colors.webpage.prefers_color_scheme_dark = True
+## Value to use for `prefers-color-scheme:` for websites. The "light"
+## value is only available with QtWebEngine 5.15.2+. On older versions,
+## it is the same as "auto". The "auto" value is broken on QtWebEngine
+## 5.15.2 due to a Qt bug. There, it will fall back to "light"
+## unconditionally.
+## Type: String
+## Valid values:
+##   - auto: Use the system-wide color scheme setting.
+##   - light: Force a light theme.
+##   - dark: Force a dark theme.
+c.colors.webpage.preferred_color_scheme = 'dark'
